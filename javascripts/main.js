b1088a1b13445aed89c52c1086cae7ddd92805d1
mohammad <khezri@outlook.com>
2018-10-15 21:34:10 -0500

    setup get data from JSON

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,12 @@
 // Filter fish that are "on sale"
 // Add fish to "Basket"
 
-// Load
+// Load fish
+
+$.get('../db/fishes.json')
+	.done((data) => {
+		console.log(data);
+	})
+	.fail((error) => {
+		console.error(error);
+	});
