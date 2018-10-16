c22e116b763b938433e6d2ad4b22c5a3fdc58cfa
WaysideTester <dylanmurray2119@gmail.com>
2018-10-16 18:39:48 -0500

    dynamicly printing

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,47 @@
+let arrayOfFish = [];
+
 // Filter fish that are "on sale"
-$("#main").find('.add').on("click", (event) => {
-    $(event.currentTarget).closest(".fish").hide();
-})
+
 
 $.get("../db/fishes.json")
  .done((data) => {
-     console.log(data);
+     writeFish(data.fishes);
+
  })
  .fail((error) => {
       console.error({error});
   });
 
 // Add fish to "Basket"
+
+
+// Load fish
+
+const writeFish = (arrayOfFish) => {
+    let newString = ``;
+    arrayOfFish.forEach((fish)=> {
+        newString += `<div class="${fish.onSale ? 'on-sale' : ''} fish card col-md-6 col-md-offset-3">
+            <div class="thumbnail">
+                <img src="${fish.imageSoure}"
+                    alt="" width="40%">
+            <div class="caption">
+                <h3 id="thumbnail-label">${fish.name}</h3>
+                <p>$
+                    <span class="price">${fish.basePrice}</span>
+                </p>
+            </div>
+                <div class="caption card-footer">
+                    <button class="add btn btn-danger">Add To Basket</button>
+                </div>
+            </div>
+        </div>`;
+    })
+    $("#available").append(newString);
+    addRemoveEvent();
+};
+
+const addRemoveEvent = () => {
+    $("#main").find('.add').on("click", (event) => {
+        $(event.currentTarget).closest(".fish").hide();
+    })
+}
