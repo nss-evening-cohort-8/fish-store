a83bb587b0b4ba32a91cdfcc268639e0fa39a6a3
mohammad <khezri@outlook.com>
2018-10-16 18:46:03 -0500

    making fish

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,7 +6,32 @@
 $.get('../db/fishes.json')
 	.done((data) => {
 		console.log(data);
+		writeFishes(data.fishes);
 	})
 	.fail((error) => {
 		console.error(error);
 	});
+
+const writeFishes = (arrayOfFishes) => {
+	let domString = '';
+
+	arrayOfFishes.forEach((fish) => {
+		domString += `<div class="${fish.onsSale ? 'on-sale' : ''} fish card col-md-6 col-md-offset-3">
+			<div class="thumbnail">
+				<img src="${fish.imageSoure}" alt="" width="40%">
+				<div class="caption">
+					<h3 id="thumbnail-label">${fish.name}</h3>
+					<p>$
+						<span class="price">${fish.basePrice}</span>
+					</p>
+				</div>
+				<div class="caption card-footer">
+					<button class="add btn btn-danger">Add To Basket</button>
+				</div>
+			</div>
+		</div>`;
+	});
+	$('#available').append(domString);
+};
+
+// $(domString).appendTo("#available");
