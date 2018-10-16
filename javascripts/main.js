6a7e65636221cb1cae5f9125dd8c3b6b8d914dfd
Wayne Collier <ke4tri@gmail.com>
2018-10-15 21:28:15 -0500

    I am loading fish, console.log array

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,3 +5,11 @@
 // Add fish to "Basket"
 
 // Load Fish
+$.get('../db/fishes.json')
+    .done((data) => {
+        console.log(data);
+    })
+    .fail((error) => {
+        console.error(error);
+    });    // only simicolon you have should be here
+    
