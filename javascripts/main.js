acee0b613c9d4dff8082e82e8076b8a0018ddda5
WaysideTester <dylanmurray2119@gmail.com>
2018-10-15 21:28:35 -0500

    completed

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,4 +2,13 @@
 $("#main").find('.add').on("click", (event) => {
     $(event.currentTarget).closest(".fish").hide();
 })
+
+$.get("../db/fishes.json")
+ .done((data) => {
+     console.log(data);
+ })
+ .fail((error) => {
+      console.error({error});
+  });
+
 // Add fish to "Basket"
