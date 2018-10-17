dcf36baac4817766ba9d21f327aa0206b1a50cc9
mohammad <khezri@outlook.com>
2018-10-16 19:47:21 -0500

    fish

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,52 @@
 // Filter fish that are "on sale"
 // Add fish to "Basket"
 
-// Load
+const writeFishes = (arrayOfFishes) => {
+	let domString = '';
+	arrayOfFishes.forEach((fish) => {
+		domString += `
+		  <div class="${fish.onSale ? 'on-sale' : ''} fish card col-md-6 col-md-offset-3">
+				  <div class="thumbnail">
+					  <img src="${fish.imageSoure}"
+					   alt="" width="40%">
+					  <div class="caption">
+						  <h3 id="thumbnail-label">${fish.name}</h3>
+						  <p>$
+							  <span class="price">${fish.basePrice}</span>
+						  </p>
+					  </div>
+					  <div class="caption card-footer">
+						  <button class="add btn btn-danger">Add To Basket</button>
+					  </div>
+				  </div>
+			  </div>`;
+	});
+	$('#available').append(domString);
+	bindEvents();
+};
+
+const bindEvents = () => {
+	$('.add').on('click', (e) => {});
+	$('.revove').on('click', (e) => {});
+};
+
+$('body').on('click', 'button.add', (e) => {
+	const fishToMove = $(e.target).closest('.fish');
+	$('#snagged').append(fishToMove);
+	$(e.target).text('Remove from Basket').addClass('remove').removeClass('add');
+});
+
+$('body').on('click', 'button.remove', (e) => {
+	const fishToMove = $(e.target).closest('.fish');
+	$('#available').append(fishToMove);
+	$(e.target).text('Add To Basket').addClass('add').removeClass('remove');
+});
+
+$.get('../db/fishes.json')
+	.done((data) => {
+		console.log(data);
+		writeFishes(data.fishes);
+	})
+	.fail((error) => {
+		console.error(error);
+	});
